eca4aa60832882f17d8f9faa91ffc4b507fd0324
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-01-28 15:42:17 +0100

    Stock of the day init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center p-4">
+        <h1 className="text-3xl font-bold">
+          Welcome to StockOfTheDay
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
