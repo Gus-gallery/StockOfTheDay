62a8fcd788adb55294b64d836b483613c7b641b1
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-01-28 16:40:27 +0100

    visual mockup implemented - ready for components to be made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center p-4">
-        <h1 className="text-3xl font-bold">
-          Welcome to StockOfTheDay Woop
+      <div className="flex flex-col items-center justify-center p-4 mt-12">
+        <h2 className="text-3xl font-classic text-gray-400">
+          Today's
+        </h2>
+        <h1 className="text-5xl font-classicBold text-gray-300 ">
+          Stock of The Day
         </h1>
+        <Card />
       </div>
     </>
   )
